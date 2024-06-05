3a7ddf35dc7edcb44487550f48801bfcb5874fc4
Waël <waelrais491@gmail.com>
2024-06-05 03:02:52 +0200

    Updated database setup script
    + Made dbsetup interactive
    + Changed session id primary key to auto incrementing int

--- server/dbsetup.php
+++ server/dbsetup.php
@@ -30,7 +30,8 @@ CREATE TABLE accounts (
 
 DROP TABLE IF EXISTS sessions;
 CREATE TABLE sessions (
-	sessionId BINARY(16) UNIQUE,
+	sessionId INT AUTO_INCREMENT,
+	token BINARY(16) UNIQUE,
 	userId BINARY(16),
 	ipAddress VARCHAR(45),
 	created DATETIME NOT NULL,
@@ -63,24 +64,82 @@ else
 	}
 }
 
+echo("\nDatabase created.\n\n");
+
 $req = "INSERT INTO accounts (userId, email, password, role) VALUES (UUID_TO_BIN(UUID()), ?, ?, 'admin'); -- todo? move this to an actual sql file";
 
 $sqli->query("USE arcadia");
 $stmt = $sqli->prepare($req);
 
+// * SOURCE: https://gist.github.com/scribu/5877523
+// slightly modified, couldn't find any cleaner way of doing this on windows
+function prompt_silent($prompt = "") {
+	echo($prompt);
+
+	echo("\033[107;97m");
+	$input = fgets(STDIN);
+	echo("\033[0m");
+
+	return rtrim($input, "\r\n");
+}
+
 if($stmt)
 {
-	$email = "admin@arcadia";
-	$pass = password_hash("123451", PASSWORD_DEFAULT);
+	while(true)
+	{
+		$email = readline("Enter a login email for the admin account: ");
 
-	$stmt->bind_param("ss", $email, $pass);
+		$numAt = substr_count($email, "@");
 
-	$stmt->execute();
+		if($numAt <= 0)
+			echo("\nEmail address must contain an @.\n");
+		else if($numAt > 1)
+			echo("\nEmail address may not contain multiple @ characters.\n");
+		else
+		{
+			$at = strpos($email, "@");
+			if($at <= 0)
+				echo("\nEmail address must contain at least one character before the @.\n");
+			else if($at >= strlen($email) - 1)
+				echo("\nEmail address must contain at least one character after the @.\n");
+			else
+				break;
+		}
+	}
+
+	$hash = "";
+
+	while(true)
+	{
+		$pass = "";
+
+		while($pass == "")
+			$pass = trim(prompt_silent("\nEnter a password for the admin account: "));
+
+		$con = trim(prompt_silent("\nConfirm password: "));
+		if($pass == $con)
+		{
+			$hash = password_hash($pass, PASSWORD_DEFAULT);
+			break;
+		}
+
+		echo("\nPasswords don't match.\n");
+	}
+
+	$stmt->bind_param("ss", $email, $hash);
+
+	$res = $stmt->execute();
 	$stmt->close();
+
+	if(!$res)
+	{
+		echo("Error saving admin account: " . $sqli->error . "\n\n");
+		return;
+	}
 }
 else
 {
 	echo("Error creating admin account: " . $sqli->error . "\n\n");
 }
 
-echo("\nDatabase created.\n\n");
+echo("\nDatabase creation complete.\n\n");
