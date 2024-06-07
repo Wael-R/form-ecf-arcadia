8bb1919b11b387ab0f7844c7a81d26863fd979cd
Waël <waelrais491@gmail.com>
2024-06-07 22:52:24 +0200

    Fixed navbar links not using absolute paths

--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand-md bg-success text-light">
 	<div class="container-fluid">
-		<a href="index.php" class="navbar-brand text-light fw-bold">Arcadia</a> <!-- todo? png logo-->
+		<a href="/index.php" class="navbar-brand text-light fw-bold">Arcadia</a> <!-- todo? png logo-->
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Ouvrir la barre de navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse justify-content-center" id="navbarContent">
 			<div class="navbar-nav nav-underline" role="navigation">
-				<a class="text-light nav-link" href="index.php" aria-current="page">Accueil</a>
-				<a class="text-light nav-link" href="services.php">Services</a>
-				<a class="text-light nav-link" href="habitats.php">Habitats</a>
-				<a class="text-light nav-link" href="animals.php">Animaux</a>
-				<a class="text-light nav-link" href="contact.php">Contact</a>
-				<a class="text-light nav-link" href="login.php">Connexion</a>
+				<a class="text-light nav-link" href="/index.php" aria-current="page">Accueil</a>
+				<a class="text-light nav-link" href="/services.php">Services</a>
+				<a class="text-light nav-link" href="/habitats.php">Habitats</a>
+				<a class="text-light nav-link" href="/animals.php">Animaux</a>
+				<a class="text-light nav-link" href="/contact.php">Contact</a>
+				<a class="text-light nav-link" href="/login.php">Connexion</a>
 			</div>
 		</div>
 		<!-- todo (php): highlight (and disable?) active page in navbar -->
