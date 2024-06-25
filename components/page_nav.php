2581f94f35269dbd316abf999c0fe2ed7fc74c61
Waël <waelrais491@gmail.com>
2024-06-25 19:13:11 +0200

    Implemented service list and page nav

---
+++ components/page_nav.php
@@ -0,0 +1,58 @@
+<?php if($count > 1): ?>
+<br><br>
+
+<div class="center">
+	<div class="btn-group">
+		<?php if($page > 1): ?>
+		<a class="btn btn-outline-success" href="?page=1">&lt;&lt;</a>
+		<a class="btn btn-outline-success" href="?page=<?= $page - 1 ?>">&lt;</a>
+		<?php else: ?>
+		<a class="btn btn-outline-success disabled">&lt;&lt;</a>
+		<a class="btn btn-outline-success disabled">&lt;</a>
+		<?php endif; ?>
+
+		<?php
+		$start = max(1, $page - 5);
+		$end = min($count, $page + 5);
+
+		if($start > 1)
+		{
+			?>
+			<a class="btn btn-outline-success disabled">...</a>
+			<?php
+		}
+
+		for($i = $start; $i <= $end; $i++)
+		{
+			if($i != $page)
+			{
+				?>
+				<a class="btn btn-outline-success" href="?page=<?= $i ?>"><?= $i ?></a>
+				<?php
+			}
+			else
+			{
+				?>
+				<a class="btn btn-success"><?= $i ?></a>
+				<?php
+			}
+		}
+	
+		if($end < $count)
+		{
+			?>
+			<a class="btn btn-outline-success disabled">...</a>
+			<?php
+		}
+		?>
+
+		<?php if($page < $count): ?>
+		<a class="btn btn-outline-success" href="?page=<?= $page + 1 ?>">&gt;</a>
+		<a class="btn btn-outline-success" href="?page=<?= $count ?>">&gt;&gt;</a> 
+		<?php else: ?>
+		<a class="btn btn-outline-success disabled">&gt;</a>
+		<a class="btn btn-outline-success disabled">&gt;&gt;</a> 
+		<?php endif; ?>
+	</div>
+</div>
+<?php endif; ?>
--- services.php
+++ services.php
@@ -1,3 +1,9 @@
+<?php
+require_once("./server/auth.php");
+$sqli = new mysqli($config->sql->hostname, $config->sql->username, $config->sql->password, "arcadia", $config->sql->port);
+$page = $_GET["page"] ?? 1;
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,7 +13,45 @@
 	<div class="main-container">
 		<?php include("./components/navbar.php"); ?>
 
-		// todo: services
+		<br><br>
+
+		<div class="main-row px-2 px-sm-5">
+			<h2 class="text-success fw-bold">Services</h2>
+			<br>
+			<div class="container px-2">
+				<div class="row row-cols-1 g-2 align-items-start justify-content-center">
+					<?php
+						$pageSize = 10;
+						$count = 0;
+
+						$res = $sqli->execute_query(
+							"SELECT c.count, serviceId, name, description FROM services
+								LEFT JOIN (SELECT COUNT(*) AS count FROM services) AS c ON 1
+								ORDER BY name ASC LIMIT ?, ?;",
+							[($page - 1) * $pageSize, $pageSize]);
+
+						if($res)
+						{
+							while($service = $res->fetch_row())
+							{
+								$serviceTitle = htmlspecialchars($service[2]);
+								$serviceDesc = htmlspecialchars($service[3]);
+								$count = floor(($service[0] - 1) / $pageSize) + 1;
+
+								?>
+								<div class="card card-body mb-3">
+									<h5 class="card-title main-card-line"><?= $serviceTitle ?></h5>
+									<p class="card-text"><?= $serviceDesc ?></p>
+								</div>
+								<?php
+							}
+						}
+					?>
+				</div>
+			</div>
+		</div>
+
+		<?php include("./components/page_nav.php"); ?>
 	</div>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
