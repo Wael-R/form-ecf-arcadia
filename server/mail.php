6bcb580d9114c4677ae0bee63ee765bbf4fec60d
Waël <waelrais491@gmail.com>
2024-07-03 01:38:27 +0200

    Added BCC support to sendMail

--- server/mail.php
+++ server/mail.php
@@ -1,11 +1,11 @@
 <?php
-require_once("../vendor/autoload.php");
+require_once(__DIR__ . "/../vendor/autoload.php");
 
 require_once("auth.php");
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-function sendMail($to, $subject, $content)
+function sendMail(array|string $to, string $subject, string $content)
 {
 	global $config;
 
@@ -26,7 +26,14 @@ function sendMail($to, $subject, $content)
 	$mail->Password = $config->mail->password;
 
 	$mail->setFrom($config->mail->username);
-	$mail->addAddress($to);
+
+	if(is_array($to))
+	{
+		foreach($to as $email)
+			$mail->addBCC($email);
+	}
+	else
+		$mail->addAddress($to);
 
 	$mail->CharSet = "UTF-8";
 	$mail->Subject = $subject;
